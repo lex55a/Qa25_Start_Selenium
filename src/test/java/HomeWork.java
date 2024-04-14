ac45358a402221a637222d564b73479fa9e0a9c8
lex55a <166326394+lex55a@users.noreply.github.com>
2024-04-14 20:58:15 +0300

    add HomeWork

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class HomeWork {
 
@@ -17,6 +21,12 @@ public class HomeWork {
     @Test
     public void login(){
         //by tag name
+        WebElement a = wd.findElement(By.tagName("a"));
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+        WebElement input = wd.findElement(By.tagName("input"));
+        WebElement input1 = wd.findElement(By.tagName("input"));
+        WebElement button = wd.findElement(By.tagName("button"));
+        WebElement button1 = wd.findElement(By.tagName("button"));
     }
 
 }
